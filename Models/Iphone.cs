f29f1f242fb218d095ecef14248f8c839b1f91f3
ZKN599 <j2267883@gmail.com>
2024-09-22 20:05:06 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models 
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+
+     public override void InstalarAplicativo(string nomeApp) {
+
+        
+
+     
+     }
+
+
+     
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
